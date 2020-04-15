dd0b228f6feb2a0a9a04ec9cfa5db062468ef681
Deusald <deusald@gmail.com>
2020-04-15 09:15:32 +0200

    feat(TypeVector3): created type vector3

---
+++ SharpBox2D/SharpBox2D/Utils/TypeVector3.cs
@@ -0,0 +1,106 @@
+// MIT License
+
+// SharpBox2D:
+// Copyright (c) 2020 Adam "Deusald" Orliński
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// ReSharper disable InconsistentNaming
+namespace SharpBox2D
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public struct TypeVector3<T> : IEnumerable<T>
+    {
+        #region Public Variables
+
+        public T x;
+        public T y;
+        public T z;
+
+        #endregion Public Variables
+
+        #region Public Methods
+
+        #region Enumerable
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            yield return x;
+            yield return y;
+            yield return z;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion Enumerable
+
+        #region Operators
+
+        public T this[int key]
+        {
+            get
+            {
+                switch (key)
+                {
+                    case 0:
+                        return x;
+                    case 1:
+                        return y;
+                    case 2:
+                        return z;
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                switch (key)
+                {
+                    case 0:
+                    {
+                        x = value;
+                        break;
+                    }
+                    case 1:
+                    {
+                        y = value;
+                        break;
+                    }
+                    case 2:
+                    {
+                        z = value;
+                        break;
+                    }
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        #endregion Operators
+
+        #endregion Public Methods
+    }
+}
