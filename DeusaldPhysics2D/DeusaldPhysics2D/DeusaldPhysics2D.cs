fb796a0001efad6daea4d168793a2746e3a09262
Deusald <deusald@gmail.com>
2021-11-26 19:13:00 +0100

    version(DeusaldPhysics2D): updated library version to 0.9.2

--- DeusaldPhysics2D/DeusaldPhysics2D/DeusaldPhysics2D.cs
+++ DeusaldPhysics2D/DeusaldPhysics2D/DeusaldPhysics2D.cs
@@ -31,7 +31,7 @@ namespace DeusaldPhysics2D
     {
         public static bool IsInitialized { get; internal set; }
         
-        public static readonly Version Version = new Version(0, 9, 1);
+        public static readonly Version Version = new Version(0, 9, 2);
 
         public static IPhysics2DControl CreateNewPhysics(uint physicsStepsPerSec, Vector2 gravity)
         {
