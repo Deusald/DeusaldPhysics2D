b2b05f2a865211cb1dd442bd1053f381329cf84f
Deusald <deusald@gmail.com>
2021-12-06 01:50:06 +0100

    version(Project): updated library to version 1.0.2

--- DeusaldPhysics2D/DeusaldPhysics2D/DeusaldPhysics2D.cs
+++ DeusaldPhysics2D/DeusaldPhysics2D/DeusaldPhysics2D.cs
@@ -31,7 +31,7 @@ namespace DeusaldPhysics2D
     {
         public static bool IsInitialized { get; internal set; }
         
-        public static readonly Version Version = new Version(1, 0, 1);
+        public static readonly Version Version = new Version(1, 0, 2);
 
         private static bool _IsBox2dNative;
 
