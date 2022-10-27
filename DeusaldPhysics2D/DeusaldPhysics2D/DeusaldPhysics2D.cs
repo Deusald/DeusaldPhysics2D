55bb5bbf4de4e8556d2c7589d7945c90b908e489
Deusald <Deusald@gmail.com>
2022-10-28 00:46:30 +0200

    feat(Dependencies): changed back System.Runtime.CompilerServices.Unsafe to .net6

--- DeusaldPhysics2D/DeusaldPhysics2D/DeusaldPhysics2D.cs
+++ DeusaldPhysics2D/DeusaldPhysics2D/DeusaldPhysics2D.cs
@@ -31,7 +31,7 @@ namespace DeusaldPhysics2D
     {
         public static bool IsInitialized { get; internal set; }
         
-        public static readonly Version Version = new Version(1, 0, 4);
+        public static readonly Version Version = new Version(1, 0, 5);
 
         private static bool _IsBox2dNative;
 
